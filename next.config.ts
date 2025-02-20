c580726781973cd88e4a86be9e251da663902f18
Henrique <miguelhenrique4922@gmail.com>
2025-02-20 19:07:05 -0300

    .

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/portifolio"
+  basePath: "/portifolio",
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
